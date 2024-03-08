86e8b61e2e2f2faffd71cd8e97d0aa9550cd613f
R1ckyH <836650357ho@gmail.com>
2024-03-08 09:04:44 +0800

    Delete data.json

--- formatting.py
+++ formatting.py
@@ -1,5 +1,5 @@
 def format_item(data):
-    result = f"```item: {data['url_name']} ({data['item_name']})\n"
+    result = f"```{data['url_name']} ({data['item_name']})\n"
     result += f"price: {(str(data['platinum']) + 'p').ljust(19, ' ')}, quantity: {data['quantity']}\n"
     result += f"user: {data['user']['ingame_name'].ljust(20, ' ')}, locate: {data['user']['locale'].ljust(7, ' ')}, like:{data['user']['reputation']}```\n"
     return result
